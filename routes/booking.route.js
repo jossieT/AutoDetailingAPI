726ff6a410dd1dbf1a66fd6b14e2787847aa97b6
jossieT <joseteshe2017@gmail.com>
2026-05-30 17:16:08 +0300

    fix: solving booking issue for upload issue

--- routes/booking.route.js
+++ routes/booking.route.js
@@ -482,19 +482,19 @@ router.get('/api/available-slots', bookingController.getAvailableSlots);
 router.get('/api/all-working-hours', bookingController.getWorkingHoursBreakdown);
 
 
-const parseNestedBody = (req, res, next) => {
-    ['clientDetails', 'vehicleDetails', 'location'].forEach(field => {
-        if (req.body[field] && typeof req.body[field] === 'string') {
-            try {
-                req.body[field] = JSON.parse(req.body[field]);
-            } catch (e) {}
-        }
-    });
-    next();
-};
+// const parseNestedBody = (req, res, next) => {
+//     ['clientDetails', 'vehicleDetails', 'location'].forEach(field => {
+//         if (req.body[field] && typeof req.body[field] === 'string') {
+//             try {
+//                 req.body[field] = JSON.parse(req.body[field]);
+//             } catch (e) {}
+//         }
+//     });
+//     next();
+// };
 // Create a new booking
 router.post('/api/bookings',
-    parseNestedBody,
+    //parseNestedBody,
     validate(bookingValidation.createBookingSchema),
     uploadBookingImages.array('images', 5),
     bookingController.createBooking
